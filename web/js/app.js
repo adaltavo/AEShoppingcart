4cb17fa63858a36cfa1c183330c3869b57a8a833
adaltavo <tavo050894@hotmail.com>
2016-11-02 22:58:09 -0700

    Codigo de prueba para simplecart.js(favorito) y smartcart.js agregado

---
+++ web/js/app.js
@@ -0,0 +1,66 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function () {
+    /*SmartCart
+     $('#SmartCart').smartCart ({
+     selected: 0, // 0 = produts list, 1 = cart  
+     resultName: 'products_selected[]',
+     enableImage: true,
+     enableImageTooltip: true,
+     enableSearch: true,
+     enableCategoryFilter: true,
+     productItemTemplate:'<strong><%=pname%></strong><br />Category:<%=pcategory%><br/><small><%=pdesc%></small><br/><strong>Price:<%=pprice%></strong>',
+     //cartItemTemplate:'<strong><%=pname%></strong>',
+     // Events
+     onAdd: null,
+     onAdded: null,
+     onRemove: null,
+     onRemoved: null,
+     onUpdate: null,
+     onUpdated: null,
+     onCheckout: null
+     });
+     */
+    simpleCart({
+        cartStyle: "table",
+        cartColumns: [
+            {attr: "name", label: "Nombre"},
+            {attr: "size", label: "Talla"},
+            {attr: "price", label: "Precio", view: 'currency'},
+            {view: "decrement", label: false, text: "-"},
+            {attr: "quantity", label: "Cantidad"},
+            {view: "increment", label: false, text: "+"},
+            {attr: "total", label: "SubTotal", view: 'currency'},
+            {view: "remove", text: "Remover", label: false}
+        ],
+        checkout: {
+            type: "PayPal",
+            email: "you@yours.com"
+        },
+        tax: 0.075,
+        currency: "MXN",
+        /*
+        afterAdd: function () {
+            console.log("huehuehdushdfjshdjfskdj");
+            //$('.simpleCart_items table').addClass();
+            $('.simpleCart_items table').addClass('table table-hover');
+            $(".simpleCart_items table tr").wrapAll('<tbody></tbody>');
+        },
+        
+        */
+       /*
+        afterRemove : function () {
+            console.log("huehuehdushdfjshdjfskdj");
+            //$('.simpleCart_items table').addClass();
+            $('.simpleCart_items table').addClass('table table-hover');
+            $(".simpleCart_items table tr").wrapAll('<tbody></tbody>');
+        }
+        */
+    });
+
+
+});
+
